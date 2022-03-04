3eba790c0fe2c36eb80043d0857a7a1f05ab8047
Ezechi Njoku <Nnodim95@gmail.com>
2022-03-04 00:03:34 -0500

    added sign up and sign in page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import './App.css';
 import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
+import SignIn from './Pages/SignIn';
+import SignUp from './Pages/SignUp';
 import Home from './Pages/Home';
 import About from './Pages/About';
+import AppBar from './Components/ButtonAppBar';
 
 function App() {
     return (
         <Router>
+            <AppBar />
             <Routes>
-                <Route path="/aboutUs" element={<About />} />
+                <Route path="/about-us" element={<About />} />
+                <Route path="/sign-up" element={<SignUp />} />
+                <Route path="/sign-in" element={<SignIn />} />
                 <Route exact path="/" element={<Home />} />
             </Routes>
         </Router>
