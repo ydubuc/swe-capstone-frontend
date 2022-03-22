3af76ea9470599c7090c5b75d84aff9939329101
Gopi Macha <gmacha@students.kennesaw.edu>
2022-03-22 10:52:32 -0400

    api from front-end

--- src/components/login.component.js
+++ src/components/login.component.js
@@ -1,9 +1,14 @@
 import React from "react";
-
+import axios from 'axios';
+import { useHistory } from "react-router-dom";
+let  unreadMessages = false;
 function Login() {
+  const history = useHistory();
   const [state, setState] = React.useState({
     email: "",
     password: "",
+    created:false
+  
   });
   function handleChange(evt) {
     const value = evt.target.value;
@@ -12,9 +17,33 @@ function Login() {
       [evt.target.name]: value,
     });
   }
+  
+  const handleSubmit = event => {
+    event.preventDefault();
+    axios.post(" https://swe-capstone-backend.herokuapp.com/auth/sign-in", {
+      email: state.email,
+      password: state.password
+    })
+    .then((response) => {
+      console.log("hi")
+      console.log(response);
+      unreadMessages=true;
+      console.log( unreadMessages)
+      // setState({
+      //   ...state,
+      //  created: true
+      // });
+      history.push(
+        {
+            pathname:"/tickets",
+            // state:{detail:response.data}
+    });
+    });
+  };
   console.log(state);
   return (
-    <form>
+    <div>
+<form onSubmit={handleSubmit}>
       <h3>Sign In</h3>
       <div className="form-group">
         <label>Email address</label>
@@ -48,13 +77,25 @@ function Login() {
           </label>
         </div>
       </div>
-      <button type="submit" className="btn btn-primary btn-block">
+      <button type="submit" className="btn btn-primary btn-block" >
         Submit
       </button>
+ 
       <p className="forgot-password text-right">
         Forgot <a href="#">password?</a>
       </p>
+      {unreadMessages &&
+    <p>
+      You have  unread messages.
+    </p>
+    
+  }
     </form>
+    {/* <div className="container">
+      {state.created && <p>sucess</p>}
+    </div> */}
+    </div>
+    
   );
 }
 
